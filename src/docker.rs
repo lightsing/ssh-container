5b31f22a4d69248e7f8d17e8c106315eba246e57
lightsing <light.tsing@gmail.com>
2021-05-10 13:59:34 +0800

    refactor

---
+++ src/docker.rs
@@ -0,0 +1,155 @@
+use std::hash::Hash;
+use std::io::{stderr, stdout, Write};
+
+use bollard::container::{CreateContainerOptions, LogOutput, RemoveContainerOptions};
+use bollard::exec::{CreateExecOptions, ResizeExecOptions, StartExecResults};
+use bollard::image::CreateImageOptions;
+use bollard::{container, Docker};
+use futures_util::{StreamExt, TryStreamExt};
+use once_cell::sync::OnceCell;
+use serde::Serialize;
+use termion::raw::IntoRawMode;
+use termion::{get_tty, terminal_size};
+use tokio::fs::File;
+use tokio::task::spawn;
+
+static DOCKER: OnceCell<Docker> = OnceCell::new();
+
+#[derive(Debug)]
+pub struct Container<'d> {
+    id: String,
+    docker: &'d Docker,
+}
+
+#[derive(Debug)]
+pub struct Exec<'d> {
+    id: String,
+    docker: &'d Docker,
+}
+
+/// Init the singleton docker instance.
+pub fn init(docker: Docker) {
+    DOCKER.set(docker).unwrap()
+}
+
+/// Get the default docker instance.
+/// This will panic if docker instance is not set.
+pub fn default() -> &'static Docker {
+    DOCKER.get().unwrap()
+}
+
+pub async fn pull<T>(image: T) -> Result<(), bollard::errors::Error>
+where
+    T: Into<String> + Serialize + Default,
+{
+    default()
+        .create_image(
+            Some(CreateImageOptions {
+                from_image: image,
+                ..Default::default()
+            }),
+            None,
+            None,
+        )
+        .try_collect::<Vec<_>>()
+        .await?;
+    Ok(())
+}
+
+impl<'d> Container<'d> {
+    pub async fn new<T, N>(
+        config: container::Config<T>,
+        options: Option<CreateContainerOptions<N>>,
+        docker: Option<&'d Docker>,
+    ) -> Result<Container<'d>, bollard::errors::Error>
+    where
+        T: Into<String> + Eq + Hash + Serialize,
+        N: Into<String> + Serialize,
+    {
+        let docker = match docker {
+            None => default(),
+            Some(docker) => docker,
+        };
+        let id = docker.create_container::<N, T>(options, config).await?.id;
+        Ok(Self { id, docker })
+    }
+
+    pub async fn start(&self) -> Result<(), bollard::errors::Error> {
+        self.docker.start_container::<String>(&self.id, None).await
+    }
+
+    pub async fn exec<T>(
+        &self,
+        options: CreateExecOptions<T>,
+    ) -> Result<Exec<'d>, bollard::errors::Error>
+    where
+        T: Into<String> + Serialize,
+    {
+        let response = self.docker.create_exec(&self.id, options).await?;
+        Ok(Exec {
+            id: response.id,
+            docker: self.docker,
+        })
+    }
+
+    pub async fn remove(self) -> Result<(), bollard::errors::Error> {
+        self.docker
+            .remove_container(
+                &self.id,
+                Some(RemoveContainerOptions {
+                    force: true,
+                    ..Default::default()
+                }),
+            )
+            .await?;
+        Ok(())
+    }
+}
+
+impl<'d> Exec<'d> {
+    pub async fn attach(&self) -> Result<(), bollard::errors::Error> {
+        if let StartExecResults::Attached {
+            mut output,
+            mut input,
+        } = self.docker.start_exec(&self.id, None).await?
+        {
+            spawn(async move {
+                let mut tty = File::from_std(get_tty().unwrap());
+                tokio::io::copy(&mut tty, &mut input).await.unwrap();
+            });
+
+            let tty_size = terminal_size()?;
+            self.docker
+                .resize_exec(
+                    &self.id,
+                    ResizeExecOptions {
+                        height: tty_size.1,
+                        width: tty_size.0,
+                    },
+                )
+                .await?;
+
+            // set stdout in raw mode so we can do tty stuff
+            let stdout = stdout();
+            let mut stdout = stdout.lock().into_raw_mode()?;
+            let stderr = stderr();
+            let mut stderr = stderr.lock().into_raw_mode()?;
+
+            // pipe docker exec output into stdout
+            while let Some(Ok(output)) = output.next().await {
+                match output {
+                    LogOutput::StdErr { message } => {
+                        stdout.write_all(message.as_ref())?;
+                        stdout.flush()?;
+                    }
+                    LogOutput::StdOut { message } => {
+                        stderr.write_all(message.as_ref())?;
+                        stderr.flush()?;
+                    }
+                    _ => (),
+                }
+            }
+        }
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,19 @@
-use anyhow::Result;
-use bollard::container::{Config, RemoveContainerOptions, LogOutput};
-use bollard::Docker;
-
-use bollard::exec::{CreateExecOptions, ResizeExecOptions, StartExecResults};
-use bollard::image::CreateImageOptions;
-use futures_util::{StreamExt, TryStreamExt};
-use std::io::{stdout, Write, stderr};
-use termion::raw::IntoRawMode;
-use termion::{terminal_size, get_tty};
-use tokio::fs::File;
-use tokio::task::spawn;
-use tokio::io::AsyncWriteExt;
 use std::process::exit;
 
-const IMAGE: &'static str = "ubuntu:focal";
+use anyhow::Result;
+use bollard::container::Config;
+use bollard::exec::CreateExecOptions;
+use bollard::Docker;
+
+const IMAGE: &str = "ubuntu:focal";
+
+mod docker;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let docker = Docker::connect_with_unix_defaults()?;
+    docker::init(Docker::connect_with_unix_defaults().unwrap());
 
-    docker
-        .create_image(
-            Some(CreateImageOptions {
-                from_image: IMAGE,
-                ..Default::default()
-            }),
-            None,
-            None,
-        )
-        .try_collect::<Vec<_>>()
-        .await?;
+    docker::pull(IMAGE).await?;
 
     let container_config = Config {
         image: Some(IMAGE),
@@ -37,79 +21,23 @@ async fn main() -> Result<()> {
         ..Default::default()
     };
 
-    let id = docker
-        .create_container::<&str, &str>(None, container_config)
-        .await?
-        .id;
-    docker.start_container::<String>(&id, None).await?;
+    let container = docker::Container::new::<_, &str>(container_config, None, None).await?;
+    container.start().await?;
 
-    let exec = docker
-        .create_exec(
-            &id,
-            CreateExecOptions {
-                attach_stdout: Some(true),
-                attach_stderr: Some(true),
-                attach_stdin: Some(true),
-                tty: Some(true),
-                cmd: Some(vec!["bash"]),
-                working_dir: Some("/root"),
-                ..Default::default()
-            },
-        )
-        .await?
-        .id;
-    if let StartExecResults::Attached {
-        mut output,
-        mut input,
-    } = docker.start_exec(&exec, None).await?
-    {
-        spawn(async move {
-            let mut tty = File::from_std(get_tty().unwrap());
-            tokio::io::copy(&mut tty, &mut input).await.unwrap();
-        });
-
-        let tty_size = terminal_size()?;
-        docker
-            .resize_exec(
-                &exec,
-                ResizeExecOptions {
-                    height: tty_size.1,
-                    width: tty_size.0,
-                },
-            )
-            .await?;
-
-        // set stdout in raw mode so we can do tty stuff
-        let stdout = stdout();
-        let mut stdout = stdout.lock().into_raw_mode()?;
-        let stderr = stderr();
-        let mut stderr = stderr.lock().into_raw_mode()?;
-
-        // pipe docker exec output into stdout
-        while let Some(Ok(output)) = output.next().await {
-            match output {
-                LogOutput::StdErr { message } => {
-                    stdout.write_all(message.as_ref())?;
-                    stdout.flush()?;
-                }
-                LogOutput::StdOut { message } => {
-                    stderr.write_all(message.as_ref())?;
-                    stderr.flush()?;
-                }
-                _ => ()
-            }
-        }
-    }
-
-    docker
-        .remove_container(
-            &id,
-            Some(RemoveContainerOptions {
-                force: true,
-                ..Default::default()
-            }),
-        )
+    let exec = container
+        .exec(CreateExecOptions {
+            attach_stdout: Some(true),
+            attach_stderr: Some(true),
+            attach_stdin: Some(true),
+            tty: Some(true),
+            cmd: Some(vec!["bash"]),
+            working_dir: Some("/root"),
+            ..Default::default()
+        })
         .await?;
 
+    exec.attach().await?;
+
+    container.remove().await?;
     exit(0);
 }
